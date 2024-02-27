c5bc3955a597259de6c8088a00841d04c3784aac
ZzzYL9 <1574775812@qq.com>
2024-02-27 20:35:42 +0800

    完成IO线程组

---
+++ rocket/net/io_thread_group.h
@@ -0,0 +1,35 @@
+#ifndef RPC_IO_THREAD_GROUP_H
+#define RPC_IO_THREAD_GROUP_H
+
+#include <vector>
+#include "common/log.h"
+#include "net/io_thread.h"
+
+namespace rocket {
+
+class IOThreadGroup {
+
+ public:
+  IOThreadGroup(int size);
+
+  ~IOThreadGroup();
+
+  void start();
+
+  void join();
+
+  IOThread* getIOThread();
+
+ private:
+
+  int m_size {0};
+  std::vector<IOThread*> m_io_thread_groups;
+
+  int m_index {0};
+
+};
+
+}
+
+
+#endif
