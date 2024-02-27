9d257ac2587457e22f5478440f8f5dfda816c46c
ZzzYL9 <1574775812@qq.com>
2024-02-27 19:16:34 +0800

    实现IO线程

--- testcases/test_eventloop.cc
+++ testcases/test_eventloop.cc
@@ -9,7 +9,77 @@
 #include "../rocket/net/fdevent.h"
 #include "../rocket/net/eventloop.h"
 #include "../rocket/net/timer_event.h"
+#include "../rocket/net/io_thread.h"
 
+void test_io_thread() {
+
+  int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (listenfd == -1) {
+    ERRORLOG("listenfd = -1");
+    exit(0);
+  }
+
+  sockaddr_in addr;
+  memset(&addr, 0, sizeof(addr));
+
+  addr.sin_port = htons(12310);
+  addr.sin_family = AF_INET;
+  inet_aton("127.0.0.1", &addr.sin_addr);
+
+  int rt = bind(listenfd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+  if (rt != 0) {
+    ERRORLOG("bind error");
+    exit(1);
+  }
+
+  rt = listen(listenfd, 100);
+  if (rt != 0) {
+    ERRORLOG("listen error");
+    exit(1);
+  }
+
+  rocket::FdEvent event(listenfd);
+  event.listen(rocket::FdEvent::IN_EVENT, [listenfd](){
+    sockaddr_in peer_addr;
+    socklen_t addr_len = sizeof(peer_addr);
+    memset(&peer_addr, 0, sizeof(peer_addr));
+    int clientfd = accept(listenfd, reinterpret_cast<sockaddr*>(&peer_addr), &addr_len);
+
+    DEBUGLOG("success get client fd[%d], peer addr: [%s:%d]", clientfd, inet_ntoa(peer_addr.sin_addr), ntohs(peer_addr.sin_port));
+
+  });
+
+  int i = 0;
+  rocket::TimerEvent::s_ptr timer_event = std::make_shared<rocket::TimerEvent>(
+    1000, true, [&i]() {
+      INFOLOG("trigger timer event, count=%d", i++);
+    }
+  );
+
+
+  rocket::IOThread io_thread;
+
+
+  io_thread.getEventLoop()->addEpollEvent(&event);
+  io_thread.getEventLoop()->addTimerEvent(timer_event);
+  io_thread.start();
+
+  io_thread.join();
+
+//   rocket::IOThreadGroup io_thread_group(2);
+//   rocket::IOThread* io_thread = io_thread_group.getIOThread();
+//   // io_thread->getEventLoop()->addEpollEvent(&event);
+//   io_thread->getEventLoop()->addTimerEvent(timer_event);
+
+//   rocket::IOThread* io_thread2 = io_thread_group.getIOThread();
+//   io_thread2->getEventLoop()->addTimerEvent(timer_event);
+
+//   io_thread_group.start();
+
+//   io_thread_group.join();
+
+
+}
 
 int main() {
 
@@ -17,54 +87,56 @@ int main() {
 
     rocket::Logger::InitGlobalLogger(0);
 
-    rocket::EventLoop* eventloop = new rocket::EventLoop();
+    test_io_thread();
 
-    int listenfd = socket(AF_INET, SOCK_STREAM, 0);
-    if(listenfd == -1){
-        ERRORLOG("listenfd = -1");
-        exit(0);
-    }
-    sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
+    // rocket::EventLoop* eventloop = new rocket::EventLoop();
 
-    addr.sin_port = htons(12348);
-    addr.sin_family = AF_INET;
-    inet_aton("127.0.0.1", &addr.sin_addr);
+    // int listenfd = socket(AF_INET, SOCK_STREAM, 0);
+    // if(listenfd == -1){
+    //     ERRORLOG("listenfd = -1");
+    //     exit(0);
+    // }
+    // sockaddr_in addr;
+    // memset(&addr, 0, sizeof(addr));
 
-    int rt = bind(listenfd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
-    if( rt != 0){
-        ERRORLOG("bind error");
-        exit(1);
-    }
+    // addr.sin_port = htons(12348);
+    // addr.sin_family = AF_INET;
+    // inet_aton("127.0.0.1", &addr.sin_addr);
 
-    rt = listen(listenfd, 100);
-    if(rt != 0){
-        ERRORLOG("listen error");
-        exit(1);
-    }
+    // int rt = bind(listenfd, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
+    // if( rt != 0){
+    //     ERRORLOG("bind error");
+    //     exit(1);
+    // }
 
-    rocket::FdEvent event(listenfd);
-    event.listen(rocket::FdEvent::IN_EVENT, [listenfd](){
-        sockaddr_in peer_addr;
-        socklen_t addr_len = sizeof(peer_addr);
-        memset(&peer_addr, 0, sizeof(peer_addr));
-        int clientfd = accept(listenfd, reinterpret_cast<sockaddr*>(&peer_addr), &addr_len);
+    // rt = listen(listenfd, 100);
+    // if(rt != 0){
+    //     ERRORLOG("listen error");
+    //     exit(1);
+    // }
 
-        DEBUGLOG("success get client fd[%d], peer addr: [%s:%d]", clientfd, inet_ntoa(peer_addr.sin_addr), ntohs(peer_addr.sin_port));
-    });
+    // rocket::FdEvent event(listenfd);
+    // event.listen(rocket::FdEvent::IN_EVENT, [listenfd](){
+    //     sockaddr_in peer_addr;
+    //     socklen_t addr_len = sizeof(peer_addr);
+    //     memset(&peer_addr, 0, sizeof(peer_addr));
+    //     int clientfd = accept(listenfd, reinterpret_cast<sockaddr*>(&peer_addr), &addr_len);
 
-    eventloop->addEpollEvent(&event);
+    //     DEBUGLOG("success get client fd[%d], peer addr: [%s:%d]", clientfd, inet_ntoa(peer_addr.sin_addr), ntohs(peer_addr.sin_port));
+    // });
 
-    int i = 0;
-    rocket::TimerEvent::s_ptr timer_event = std::make_shared<rocket::TimerEvent>(
-        1000, true, [&i](){
-            INFOLOG("trigger timer event, count = %d", i++);
-        }
-    );
+    // eventloop->addEpollEvent(&event);
 
-    eventloop->addTimerEvent(timer_event);
+    // int i = 0;
+    // rocket::TimerEvent::s_ptr timer_event = std::make_shared<rocket::TimerEvent>(
+    //     1000, true, [&i](){
+    //         INFOLOG("trigger timer event, count = %d", i++);
+    //     }
+    // );
 
-    eventloop->loop();
+    // eventloop->addTimerEvent(timer_event);
+
+    // eventloop->loop();
 
     return 0;
 }
